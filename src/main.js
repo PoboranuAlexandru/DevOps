204d79dcaf26d8e8e102d90d0e39cb962c7bbaf2
PoboranuAlexandru <36866799+PoboranuAlexandru@users.noreply.github.com>
2021-12-08 00:21:37 +0200

    add sentry

--- src/main.js
+++ src/main.js
@@ -6,3 +6,38 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+import { createApp } from "vue";
+import { createRouter } from "vue-router";
+import * as Sentry from "@sentry/vue";
+import { Integrations } from "@sentry/tracing";
+import HelloWorld from './components/HelloWorld.vue'
+
+const app = createApp(App);
+const router = createRouter({
+  mode: 'history',
+  routers: [
+    {
+      path: '/',
+      component: HelloWorld
+    }
+  ]
+});
+
+Sentry.init({
+  app,
+  dsn: "https://e580357ef7194376a083a1c586f2e36d@o1086016.ingest.sentry.io/6097687",
+  integrations: [
+    new Integrations.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+app.use(router);
+app.mount("#app");
